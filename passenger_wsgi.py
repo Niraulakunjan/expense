46b86a6b9d27a497bab7bcffdb17e68f0bc96e8e
Kunjan Niraula <niraula67kunjan@gmail.com>
2026-03-17 00:12:38 +0545

    debug: redirect stderr in passenger_wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,13 +1,16 @@
 import sys
 import os
 
+# Redirect stderr to a log file for better debugging
+path = os.path.dirname(os.path.abspath(__file__))
+sys.stderr = open(os.path.join(path, 'passenger_stderr.log'), 'a')
+
 # Bare minimum test application to verify if Passenger is running this file
 def application(environ, start_response):
     status = '200 OK'
     output = b"Hello from Passenger! If you see this, the file is being executed correctly.\n"
     
     # Log to a file to be 100% sure
-    path = os.path.dirname(os.path.abspath(__file__))
     with open(os.path.join(path, 'passenger_test.log'), 'a') as f:
         f.write("Application called successfully!\n")
         
@@ -15,3 +18,4 @@ def application(environ, start_response):
                         ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
     return [output]
+
