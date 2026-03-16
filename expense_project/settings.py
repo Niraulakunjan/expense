b54689e2a0a9decf5e3e21b18f779d658c43ac0c
Kunjan Niraula <niraula67kunjan@gmail.com>
2026-03-16 22:50:23 +0545

    update ALLOWED_HOSTS for production

--- expense_project/settings.py
+++ expense_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^#hhbprau36roxegm@x3en^k##5+kv)5aygqw3l7mz3qdwur-+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['api.expense.kunjanniraula.com.np', 'localhost', '127.0.0.1']
 
 
 # Application definition
